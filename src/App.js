3fb02af286cf56e03cfea6539c848527c1b8c9a9
thaovy1994 <letranthaovy1326@gmail.com>
2024-05-24 19:47:49 +0700

    install router bootstrap

--- src/App.js
+++ src/App.js
@@ -1,34 +1,12 @@
-import React from "react";
 import "./App.scss";
 import MyComponent from "./components/MyComponent";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="app-container">
-        <MyComponent></MyComponent>
-      </div>
-    );
-  }
-}
-
-// const App = () => {
-//   const count = useSelector(state => state.counter.count);
-//   const dispatch = useDispatch();
-
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <div>Count = {count}</div>
-//         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-//         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-//       </header>
-//     </div>
-//   );
-// }
+const App = () => {
+  return (
+    <div className="app-container">
+      <MyComponent></MyComponent>
+    </div>
+  );
+};
 
 export default App;
