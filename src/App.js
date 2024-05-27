57793adb427b7d2f31d340a86603ca89e368d1dc
thaovy1994 <letranthaovy1326@gmail.com>
2024-05-27 16:59:02 +0700

    Router - Outlet

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import { Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import "./App.scss";
 import Header from "./components/Header/Header";
 
 const App = () => {
   return (
     <div className="app-container">
-      <Header />
-      <div>
-        <button>
-          <Link to="/users">User Page</Link>
-        </button>
-        <button>
-          <Link to="/admin">Admin Page</Link>
-        </button>
+      <div className="header-container">
+        <Header />
+      </div>
+      <div className="main-container">
+        <div className="sidenav-container"></div>
+        <div className="app-content">
+          <Outlet />
+        </div>
       </div>
     </div>
   );
