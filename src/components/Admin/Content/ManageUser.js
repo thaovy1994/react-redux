9044d7515c8362c8e6864e732ecb10de885b297c
thaovy1994 <letranthaovy1326@gmail.com>
2024-05-30 23:50:14 +0700

    Upload Image File

--- src/components/Admin/Content/ManageUser.js
+++ src/components/Admin/Content/ManageUser.js
@@ -1,16 +1,15 @@
+import "./ManageUser.scss";
 import ModalCreateUser from "./ModalCreateUser";
 
 const ManageUser = (props) => {
   return (
     <div classNameName="manage-user-container">
-      <div classNameName="tittle">Manage User</div>
+      <div classNameName="title">Manage User</div>
       <div classNameName="user-content">
         <button>Add new user</button>
       </div>
-      <div classNameName="">
-        Table User
-        <ModalCreateUser />
-      </div>
+      <div classNameName="">Table User</div>
+      <ModalCreateUser />
     </div>
   );
 };
