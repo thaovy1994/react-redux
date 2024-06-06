450ad3148a4d945a1d716d8fc84d9e8484fac28e
thaovy1994 <letranthaovy1326@gmail.com>
2024-06-06 16:02:51 +0700

    Redux

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { useDispatch, useSelector } from "react-redux";
 import "./App.scss";
 import MyComponent from "./components/MyComponent";
+import { decreaseCounter, increaseCounter } from "./redux/action/counterAction";
 
 const App = () => {
+  const count = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
   return (
     <div className="app-container">
       <MyComponent></MyComponent>
+      <div>
+        Count ={count}
+        <button onClick={() => dispatch(increaseCounter())}>Increase</button>
+        <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
+      </div>
     </div>
   );
 };
