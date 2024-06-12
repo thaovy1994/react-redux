f9128b1a5f95f0a06fbbaa2805b39189f5a6a72c
thaovy1994 <letranthaovy1326@gmail.com>
2024-06-13 00:00:56 +0700

    loading bar - nprogress

--- src/utils/axiosCustomize.js
+++ src/utils/axiosCustomize.js
@@ -19,6 +19,7 @@ instance.interceptors.request.use(
     return config;
   },
   function (error) {
+    NProgress.start();
     // Do something with request error
     return error && error.response && error.response.data
       ? error.response.data
@@ -35,6 +36,7 @@ axios.interceptors.response.use(
     return response && response.data ? response.data : response;
   },
   function (error) {
+    NProgress.done();
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
     return Promise.reject(error);
